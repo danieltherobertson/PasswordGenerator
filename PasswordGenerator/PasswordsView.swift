0f4cb48165bd32b6e7ccc3a6dfc56110d694ca2d
Daniel Robertson <danieltherobertson@gmail.com>
2016-09-24 11:44:54 +0100

    Adds view to passwordCollectionView and sets up view for programmatic collection view

--- PasswordGenerator/PasswordsView.swift
+++ PasswordGenerator/PasswordsView.swift
@@ -9,6 +9,40 @@
 import UIKit
 
 class PasswordsView: UIView {
+    
+    var passwordsView: UICollectionView!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        passwordsView.frame = self.bounds
+        
+    }
+    
+    //-----------------INITIALISING THE COLLECTION VIEW INSIDE THE UIVIEW ----------------------------------------------
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 300, height: 100)
+        layout.minimumLineSpacing = 20
+        
+        passwordsView = UICollectionView(frame: self.bounds, collectionViewLayout: layout)
+        passwordsView.translatesAutoresizingMaskIntoConstraints = false
+        passwordsView.dataSource = self
+        passwordsView.delegate = self
+        
+        let nib = UINib(nibName: "gameCellNib", bundle: nil)
+        gamesView.registerNib(nib, forCellWithReuseIdentifier: "gameCell")
+        gamesView.backgroundColor = UIColor.clearColor()
+        self.addSubview(gamesView)
+        
+        //Load any existing gameSave types from gameSaves
+        loadGame()
+        
+    }
+
 
     /*
     // Only override drawRect: if you perform custom drawing.
